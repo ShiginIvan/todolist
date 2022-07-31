495eb908cb1ee3cc1bd7623df91a59e948e27cba
ShiginIvan <Shigin.ivan1997@yandex.ru>
2022-07-31 13:58:34 +0300

    initial

--- website/website/settings.py
+++ website/website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-febdbxvt_a#5r7z!1r41p2g1up0eeqncr^1zb@^6=eo_@flwer
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shiginivan.pythonanywhere.com']
 
 
 # Application definition
